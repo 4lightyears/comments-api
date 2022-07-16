01c75ac3e6506e5db01f500f1b9cd7794e6e5f8a
Varun <vsvarun@gmail.com>
2022-07-16 11:20:38 +0530

    tweaked the urls for comments

--- commentsapi/comments/urls.py
+++ commentsapi/comments/urls.py
@@ -8,7 +8,7 @@ from .views import AllComments, CommentDetail, ReplyDetail, CreateReply
 
 
 urlpatterns = [
-    path('comments', AllComments.as_view()),
+    path('', AllComments.as_view()),
     path('comment/<int:comment_id>', CommentDetail.as_view()),
     path('comment/reply', CreateReply.as_view()),
     path('comment/reply/<int:reply_id>', ReplyDetail.as_view()),
